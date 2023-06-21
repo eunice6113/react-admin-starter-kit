0a16a30ebd65ca4044c365a98dfee3a4b204abbe
lyk <lyk@ibksystem.co.kr>
2023-06-21 14:18:52 +0900

    안 쓰는 service, model 삭제

--- src/app/core/models/rpcm.ts
+++
@@ -1,7 +0,0 @@
-export interface Post {
-    rpcmNo:string;
-    efttEvntNo:string;
-    cmnyNo?:string;         //커뮤니티번호
-    rpcmCon:string;
-}
-
--- src/app/core/models/uht.ts
+++
@@ -1,7 +0,0 @@
-export interface Post {
-    emn?:string;    //직원번호
-    emm:string;     //직원명
-
-    srEmn:string;
-    srEmm:string;    
-}
